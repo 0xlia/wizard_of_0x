b7b79a4b4b4064fa1eb1e49d789868ab135ae611
0xlia <hi@0xlia.com>
2026-03-07 20:10:10 +0100

    restructure backend

--- frontend/index.js
+++ frontend/index.js
@@ -1,16 +1,13 @@
 const form1 = document.querySelector("#create_game_input");
 const form2 = document.querySelector("#join_game_input");
 
-let websocket = new WebSocket("ws://localhost:8765/");
-
-window.addEventListener("DOMContentLoaded", () => {
-	
-});
+const websocket = new WebSocket("ws://localhost:8765/");
 
 form1.addEventListener("submit", handler);
 form2.addEventListener("submit", handler);
 
 
+// handle submit 
 function handler(evt) {
 	// verhindere Standardverhalten des Formulars
 	evt.preventDefault();
@@ -20,14 +17,21 @@ function handler(evt) {
 
 	// extrahiert die Daten
 	const data = Array.from(inputs).map((input) => [input.id, input.value]);
-	
 
 	// logt die Daten in der Console des Browsers.
+	console.log(inputs);
 	console.log(data);
+
+	// checken ob verbindung noch steht
 	websocket.send(JSON.stringify(data));
+
+	// NewGame
+	// player name: 
+	// number: 
 
 }
 
+// handle message 
 websocket.addEventListener("message", ({ data }) => {
 	const event = JSON.parse(data);
 	console.log(event)
@@ -40,3 +44,6 @@ websocket.addEventListener("message", ({ data }) => {
 
 
 });
+
+
+// update Game ID 
--- gameserver/src/main.rs
+++
@@ -1,8 +0,0 @@
-use gameserver::gamelogic::WizardGame;
-
-fn main() {
-    let game = WizardGame::new(3, "spooky".to_string());
-    dbg!(game);
-}
-
-
